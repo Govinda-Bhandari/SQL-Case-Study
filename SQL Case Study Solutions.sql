afb5ae3b67c741f0c1f6b9f60af11b6202bcf2af
Govinda-Bhandari <46941777+Govinda-Bhandari@users.noreply.github.com>
2024-10-24 01:04:55 +0200

    Add files via upload

---
+++ SQL Case Study Solutions.sql
@@ -0,0 +1,50 @@
+--Formating data and keeping in correct order
+SELECT TOP (10) InvoiceNo
+      ,StockCode
+      ,Description
+      ,Quantity
+      ,FORMAT(InvoiceDate,'yyyy-MM-dd') as InvoiceDate
+      ,ROUND(UnitPrice,2) as UnitPrice
+      ,[CustomerID]
+      ,[Country]
+  FROM [DATA_DB].[dbo].retail_sales_data
+
+--Deleting records whre CustomerID is null and Negative Quantity value
+DELETE FROM retail_sales_data
+ WHERE CustomerID IS NULL
+ or Quantity < 0;
+
+--Removing Duplicates with Rank Windows Functions:
+
+WITH RankedSales AS (
+    SELECT *,
+        ROW_NUMBER() OVER (PARTITION BY InvoiceNo, 
+        StockCode, Quantity, InvoiceDate ORDER BY InvoiceNo) AS rank
+    FROM retail_sales_data
+)
+DELETE FROM RankedSales
+    WHERE rank > 1
+
+
+-- TOP 5 Product by Quantity
+SELECT TOP (5) StockCode, Description, 
+    SUM(Quantity) AS TotalQuantity
+FROM retail_sales_data
+GROUP BY StockCode, Description
+ORDER BY TotalQuantity DESC
+;
+
+--Total Revenue by Country
+SELECT Country, 
+round(SUM(Quantity * UnitPrice),2) AS TotalRevenue
+FROM retail_sales_data
+GROUP BY Country
+ORDER BY TotalRevenue DESC;
+
+--Top 3 Most Spending Customers
+SELECT Top (3) CustomerID,
+    round(SUM(Quantity * UnitPrice),2) AS TotalSpend
+FROM retail_sales_data
+WHERE CustomerID IS NOT NULL
+GROUP BY CustomerID
+ORDER BY TotalSpend DESC
